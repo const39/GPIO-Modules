a5def9440b2a244ab78bd1a6d44cf42e6f3fbf20
const39 <const8539@gmail.com>
2020-08-12 13:27:02 +0200

    Finished remote_scanner module script

--- modules/remote_scanner/remote_scanner.py
+++ modules/remote_scanner/remote_scanner.py
@@ -1,24 +1,22 @@
 """
-Python script listening to the GPIO pin n° #######.
+Python script listening to the GPIO pin n°23.
 If an input is detected (via a button push), request a scan to the defaut scanner and save the file in a directory.
 @author const39
 """
 
-from gpiozero import Button
-import subprocess
+from gpiozero import LED, Button
 from datetime import datetime
+from signal import pause
+import subprocess
+import os
 
 
-DEST_DIR = "/home/pi/scan"
-DEST_FORMAT = "tiff"
+HOME_DIR = os.getenv("HOME") 
+SCAN_DIR = "scan"
+DEST_DIR = HOME_DIR + '/' + SCAN_DIR
+DEST_FORMAT = "png"
 
-# Initialise a connection on the pin n° ########### TODO
-button = Button(5)
-button.when_pressed = scan
-
-
-# TODO add safety net to prevent the start of multiple scan at once
-
+isRunning = False
 
 def generateFilename():
     """
@@ -26,13 +24,47 @@ def generateFilename():
     """
     return "scan_" + datetime.now().strftime("%d/%m/%Y_%H:%M:%S") + "." + DEST_FORMAT
 
+def createDir():
+    """
+    Create the destination directory if it does not exist.
+    """
+
+    if not os.path.exists(DEST_DIR):
+        os.mkdir(DEST_DIR)
 
 def scan():
-    # Create the file name with the current datetime
-    filename = generateFilename()
 
-    # Request a scan
-    subprocess.call(
-        ["scanimage", "--format=" + DEST_FORMAT, ">", DEST_DIR + "/" + filename],
-        shell=False,
-    )
+    global isRunning
+    if isRunning == False:
+
+        isRunning = True
+
+        # Create the destination directory if it doesn't exist
+        createDir()
+
+        # Create the file name with the current datetime
+        filename = generateFilename()
+
+        # Blink LED to indicate that the scan is processing
+        led.blink(0.8, 0.8)
+
+        # Request a scan
+        subprocess.call(
+            ["scanimage", "--format=" + DEST_FORMAT, ">", DEST_DIR + "/" + filename],
+            shell=False,
+        )
+
+        # Switch the LED off once the process has ended
+        led.off()
+
+        isRunning = False
+
+
+## ** Main code ** ##
+button = Button(23) # GPIO 16 = BCM 23
+led = LED(24) # GPIO 18 = BCM 24
+
+button.when_pressed = scan # Callback when button pressed
+
+pause() # Pause main thread to prevent to script to end while background threads are running
+
