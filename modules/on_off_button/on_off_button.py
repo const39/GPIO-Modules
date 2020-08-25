ab05475f3b1d8175f3f2cab762bd2f645b01c468
const39 <const8539@gmail.com>
2020-08-25 21:01:20 +0200

    Fixed various things on modules

--- modules/on_off_button/on_off_button.py
+++ modules/on_off_button/on_off_button.py
@@ -1,15 +1,27 @@
 """
+
+
+
+
 Python script listening to the GPIO pin n°5.
 If an input is detected (via a button push), shutdown or wake up the Raspberry Pi.
 @author const39
 """
 
-from gpiozero import Button
+from gpiozero import Button, LED
+from signal import pause
 import subprocess
 
-# On initialise une connexion sur la broche BCM 3 (= GPIO 5)
-button = Button(3)
-button.wait_for_press()
+def halt():
+	led.off()
+	subprocess.call(["poweroff"], shell=False)
 
-subprocess.call(["halt"], shell=False)
 
+## ** Main code ** ##
+button = Button(3) # BCM 3 = GPIO 5
+led = LED(4) # BCM 4 = GPIO 7
+
+led.on()
+button.when_pressed = halt # Callback when button is pressed
+
+pause() # Pause main thread to prevent the script to end while background threads are running
